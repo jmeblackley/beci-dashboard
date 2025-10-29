6f92c2beb5d4aeec59608fd7d38fff3e47fd93b2
jamie blackley <45036156+jmeblackley@users.noreply.github.com>
2025-10-29 14:47:12 -0700

    Delete config.local.js

--- config.local.js
+++
@@ -1,7 +0,0 @@
-// Do NOT commit this file. .gitignore should list "config.local.js".
-
-window.BECI_CONFIG = {
-  apiKey: "AAPTxy8BH1VEsoebNVZXo8HurKC8LWXmfGB2HYfFZFZ5-zmTluOSVcZdMvfJTHTKmm-v147XzhBV9caO-JxkYVKbhf2MhdLnGwGcGWuVDLnABZJOZZzTHWwwORX2hf4AqU_TZJKpurRbJ84sJ9iox_2TiUHvvx6lzim0dC8RIt_eL-nVO3ZHs7eN5fjLtkP_kaIrx7CFhqcYnu54JOPXpPgIVGSWzRvc148PZkYESenAz7g.AT1_edvS32F3",
-  spatialReference: { wkid: 3832 },        // or { wkt: "..." }
-  
-};
