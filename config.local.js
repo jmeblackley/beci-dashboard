5943c4d0953e01f43686fbae48140b9a0e4365c5
jamie blackley <45036156+jmeblackley@users.noreply.github.com>
2025-12-01 12:05:22 -0800

    Update config.local.js

--- config.local.js
+++ config.local.js
@@ -1,6 +1,6 @@
 // Do NOT commit this file. .gitignore should list "config.local.js".
 window.BECI_CONFIG = {
-  apiKey: "AAPTxy8BH1VEsoebNVZXo8HurKC8LWXmfGB2HYfFZFZ5-zmTluOSVcZdMvfJTHTKmm-v147XzhBV9caO-JxkYVKbhf2MhdLnGwGcGWuVDLnABZJOZZzTHWwwORX2hf4AqU_TZJKpurRbJ84sJ9iox_2TiUHvvx6lzim0dC8RIt_eL-nVO3ZHs7eN5fjLtkP_kaIrx7CFhqcYnu54JOPXpPgIVGSWzRvc148PZkYESenAz7g.AT1_edvS32F3",
+  apiKey: "",
 
   spatialReference: { wkid: 3832 },
 
